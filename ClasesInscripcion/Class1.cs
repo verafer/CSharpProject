d6dc1d9767b2b87d0541eb4163f1ca060ddf836d
Fer <verafer343@gmail.com>
2019-09-14 09:49:28 -0400

    Agregar archivos de proyecto.

---
+++ ClasesInscripcion/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClasesInscripcion
+{
+    public class Class1
+    {
+        public void Saludar(string nombre)
+        {
+            Console.WriteLine($"Hola {nombre}! :D \n Presiona una tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SistemaInscripcionAlumnos/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClasesInscripcion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInscripcionAlumnos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Class1 saludo = new Class1();
+            saludo.Saludar("Aillen o Joel jaja");
+        }
+    }
+}
