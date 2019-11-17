fbfbbdfbb6afb4acd76f4fe63234f11d39c940bf
Aillen Duarte <aillenduar@gmail.com>
2019-11-17 12:48:38 -0300

    clase ciudad

--- ClasesInscripcion/Ciudad.cs
+++ ClasesInscripcion/Ciudad.cs
@@ -1,46 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Data;
+//using System.Data.SqlClient;
 namespace ClasesInscripcion
 {
-    public enum Departamento
-    {
-        Central,
-        Cordillera,
-        Guairá
-    }
-
     public class Ciudad : Objeto
     {
-        public string Nombre { get; set; }
-        public Departamento Departamento { get; set; }
+        public string Descripcion { get; set; }
 
         public static List<Ciudad> listaCiudades = new List<Ciudad>();
 
-        public static void AgregarCiudad(Ciudad c)
-        {
-            listaCiudades.Add(c);
-        }
 
-        public static void EditarCiudad(Ciudad c, int indice)
-        {
-            Ciudad.listaCiudades[indice] = c;
-
-        }
-        public static void EliminarCiudad(Ciudad ciudad)
-        {
-            listaCiudades.Remove(ciudad);
-        }
-
-        public static List<Ciudad> ObtenerCiudad()
-        {
-            return listaCiudades;
-        }
 
         public override string ToString()
         {
-            return Nombre + " - " + Departamento;
+            //return "R. Social: " + RazonSocial +"; " + "Direcc: " + Direccion + ";" + "Contacto: " + Contacto;
+            return Descripcion;
         }
     }
 }
---
+++ ClasesInscripcion/SqlServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace ClasesInscripcion
+{
+   public  class SqlServer
+    {
+        public static string CADENA_CONEXION = ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString;
+    }
+}
